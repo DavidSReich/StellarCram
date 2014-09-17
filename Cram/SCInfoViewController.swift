94c45a80552925df9934b8e1c9992a24fe5fb7b4
David S Reich <david@Davids-iMac.local>
2014-09-17 18:47:25 +1000

    InfoView fine for iPads.

---
+++ Cram/SCInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SCInfoViewController.swift
+//  Cram
+//
+//  Created by David S Reich on 17/09/2014.
+//  Copyright (c) 2014 Stellar Software Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SCInfoViewController : UIViewController {
+    
+    @IBAction func okButtonTouched(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
